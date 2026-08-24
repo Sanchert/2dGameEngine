8bfb59c8966b4c0734ce2270fbce3b9ca63dfe99
Alexander Kolotukhin <154746573+Sanchert@users.noreply.github.com>
2026-08-24 23:09:41 +0300

    Feat: getComponent methods for game object class

--- src/main/java/org/example/keenmarksmanfx/GameObj.java
+++ src/main/java/org/example/keenmarksmanfx/GameObj.java
@@ -6,8 +6,8 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class GameObj {
-    private HashMap<String, IComponent> byName = new HashMap<>();
-    private HashMap<Class, List<IComponent>> byType = new HashMap<>();
+    private final HashMap<String, IComponent> byName = new HashMap<>();
+    private final HashMap<Class<? extends IComponent>, List<IComponent>> byType = new HashMap<>();
 
     public void addComponent(IComponent c) {
         byName.put(NameGenerator.generateName(c.getClass()), c);
@@ -19,30 +19,35 @@ public class GameObj {
         byType.put(c.getClass(), l);
     }
 
-    public void updateComponents() {
-        byName.forEach((_, c) -> c.update());
+    @SuppressWarnings("unchecked")
+    public <T> T getComponent(String name) {
+        return (T) byName.get(name);
     }
 
-    public <T> T getComponent(String exactName) {
-        return (T) byName.get(exactName);
-    }
-
-    // По типу (ищет первый)
+    @SuppressWarnings("unchecked")
     public <T> T getComponent(Class<T> type) {
         List<IComponent> list = byType.get(type);
-        return list != null && !list.isEmpty() ? (T) list.get(0) : null;
+        return list != null && !list.isEmpty() ? (T) list.getFirst() : null;
     }
 
-    // Все компоненты типа
+    @SuppressWarnings("unchecked")
     public <T> List<T> getComponents(Class<T> type) {
         return (List<T>) byType.getOrDefault(type, Collections.emptyList());
     }
 
-    public void removeComponent(IComponent component) {
+    public void removeComponent(String name) {
+        IComponent c = byName.remove(name);
+        byType.remove(c.getClass());
     }
 
-    public <T extends IComponent> boolean removeComponentIf(Class<T> class_, Predicate<T> condition) {
-        return false;
+//    public void removeComponents(Class<? extends IComponent> type) {}
+
+//    public <T extends IComponent> boolean removeComponentIf(Class<T> class_, Predicate<T> condition) {
+//        return false;
+//    }
+
+    public void updateComponents() {
+        byName.forEach((_, c) -> c.update());
     }
 
     protected double pos_x, pos_y;
@@ -56,9 +61,11 @@ public class GameObj {
         this.pos_x = pos_x;
         this.pos_y = pos_y;
     }
+
     public void destroy() {
         destroyed = true;
     }
+
     public boolean isDestroyed() {
         return destroyed;
     }
