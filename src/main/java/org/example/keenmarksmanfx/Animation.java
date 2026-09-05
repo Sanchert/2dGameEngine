3b8ef447e8075c82cf38dfe7688d05b49bef244a
Alexander Kolotukhin <154746573+Sanchert@users.noreply.github.com>
2026-09-06 01:02:22 +0300

    Feat: new animation

--- src/main/java/org/example/keenmarksmanfx/Animation.java
+++ src/main/java/org/example/keenmarksmanfx/Animation.java
@@ -1,76 +1,40 @@
 package org.example.keenmarksmanfx;
 
-import javafx.scene.image.Image;
 import org.jetbrains.annotations.NotNull;
-
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Animation {
-    private final List<Image> spriteList;
-    private final List<Long>  frameToFrameIntervals; // TODO: changing the frame rate
-    private final long totalDuration; // TODO: changing duration
+    private final int textureId;
+    private final List<float[]> UVCrd;
+    private List<Long> frameDuration;
+    private boolean looped = false;
 
-    /// @param spriteList frames of an animation in correct order
-    /// @param duration time in millis between first and last frames
-    public Animation(@NotNull List<Image> spriteList, long duration) {
-        this.totalDuration = duration;
-        this.spriteList = spriteList;
-        this.frameToFrameIntervals = new ArrayList<>(Collections.nCopies(spriteList.size(), duration / spriteList.size()));
+    public Animation(@NotNull String fileName, @NotNull List<float[]> crd) {
+        this.textureId =  Loader.textureLoad(fileName);
+        this.UVCrd = crd;
     }
 
-    /// @param spriteList frames of an animation in correct order
-    /// @param duration time in millis between first and last frames
-    /// @param intervals frame to frame intervals
-    public Animation(@NotNull List<Image> spriteList, long duration, @NotNull ArrayList<Long> intervals) {
-        this.totalDuration = duration;
-        this.spriteList = spriteList;
-        this.frameToFrameIntervals = intervals;
+    public int getTextureId() {
+        return textureId;
     }
 
-    /// @param paths array of system paths to images in correct order
-    /// @param duration time in millis between first and last frames
-    public Animation(@NotNull ArrayList<Path> paths, long duration) {
-        this.totalDuration = duration;
-        spriteList = new ArrayList<>();
-        paths.forEach(path -> spriteList.add(ImageLoader.loadImageFromPath(path.toString())));
-        this.frameToFrameIntervals = new ArrayList<>(Collections.nCopies(spriteList.size(), duration / spriteList.size()));
+    public float[] getUVCrd(int frameIndex) {
+        return UVCrd.get(frameIndex);
     }
 
-    /// @param paths array of system paths to images in correct order
-    /// @param duration time in millis between first and last frames
-    /// @param intervals frame to frame intervals
-    public Animation(@NotNull ArrayList<Path> paths, long duration, @NotNull ArrayList<Long> intervals) {
-        this.totalDuration = duration;
-        this.frameToFrameIntervals = intervals;
-        spriteList = new ArrayList<>();
-        paths.forEach(path -> spriteList.add(ImageLoader.loadImageFromPath(path.toString())));
+    public void setFrameDuration(List<Long> frameDuration) {
+        this.frameDuration = frameDuration;
     }
 
-    /// @param paths array of system paths to images in correct order
-    /// @param duration time in millis between first and last frames
-    /// @param intervals frame to frame intervals
-    public Animation(@NotNull ArrayList<Path> paths, long duration, @NotNull ArrayList<Long> intervals, double scaleX, double scaleY) {
-        this.totalDuration = duration;
-        this.frameToFrameIntervals = intervals;
-        spriteList = new ArrayList<>();
-        paths.forEach(path -> {
-            Image img = ImageLoader.loadImageFromPath(path.toString());
-            spriteList.add(img);
-        });
+    public Long getFrameDuration(int frameIndex) {
+        return frameDuration.get(frameIndex);
     }
 
-    public Image getSprite(int index) {
-        return spriteList.get(index);
+    public void setLooping(boolean isLooped) {
+        this.looped = isLooped;
     }
 
-    public long getTotalDuration() {
-        return totalDuration;
-    }
-
-    public List<Long> getFrameToFrameIntervals() {
-        return frameToFrameIntervals;
+    public boolean isLooped() {
+        return looped;
     }
 }
