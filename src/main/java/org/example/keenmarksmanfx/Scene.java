4b4d73900c89803c00d9c7e802787ff5084051a0
Alexander Kolotukhin <154746573+Sanchert@users.noreply.github.com>
2026-09-10 01:15:22 +0300

    Feat: scene

---
+++ src/main/java/org/example/keenmarksmanfx/Scene.java
@@ -0,0 +1,23 @@
+package org.example.keenmarksmanfx;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AutoName(prefix = "Scene")
+public class Scene {
+
+    private String name;
+    public String getName() { return name; }
+
+    private final List<GameObj> sceneObjects = new ArrayList<>();
+
+    public void addObject(GameObj go) { sceneObjects.add(go); }
+    public void removeObject(GameObj go) { sceneObjects.remove(go); }
+    public List<GameObj> getSceneObjects() { return sceneObjects; }
+    public GameObj getByTag(String tag) {
+        return sceneObjects.stream()
+                .filter(go -> go.getTag().equals(tag))
+                .findFirst()
+                .orElse(null);
+    }
+}
---
+++ src/main/java/org/example/keenmarksmanfx/SceneManager.java
@@ -0,0 +1,24 @@
+package org.example.keenmarksmanfx;
+
+import java.util.HashMap;
+
+public class SceneManager {
+    private SceneManager instance = null;
+
+    private static final HashMap<String, Scene> scenes = new HashMap<>();
+    private static Scene activeScene;
+
+
+    private SceneManager() {}
+
+    public SceneManager getInstance() {
+        if (instance == null) {
+            instance = new SceneManager();
+        }
+        return instance;
+    }
+
+    public static void addScene(Scene sc) { scenes.put(sc.getName(), sc); }
+    public static Scene getActiveScene() { return activeScene; }
+    public static void loadScene(String sceneName) { activeScene = scenes.get(sceneName); }
+}
